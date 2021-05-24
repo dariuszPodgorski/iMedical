c3b9e8028fa9c2c9dfd9751936829c8c94fd8be6
dariuszPod <dariuszpodgorski94@gmail.com>
2021-05-24 22:14:37 +0200

    Dodanie kontrolera specjalizacja Dodanie automappera

---
+++ iMedicalAPI/iMedicalAPI/Controllers/SpecializationController.cs
@@ -0,0 +1,46 @@
+﻿using iMedicalAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iMedicalAPI.Controllers
+{
+    [Route("api/specializations")]
+    public class SpecializationController : ControllerBase
+    {
+        private readonly iMedicalContext _dbContext;
+
+        public SpecializationController(iMedicalContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Specjalizacja>> GetAll()
+        {
+            var specializations = _dbContext
+                .Specjalizacjas
+                .ToList();
+
+            return Ok(specializations);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Specjalizacja> Get([FromRoute] int id)
+        {
+            var specializations = _dbContext
+                .Specjalizacjas
+                .FirstOrDefault(p => p.IdSpecjalizacja == id);
+
+            if (specializations is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(specializations);
+        }
+    }
+}
