6f59de13ef6d730552d87e709b95c29ccb558d6a
dariuszPod <dariuszpodgorski94@gmail.com>
2021-05-24 19:41:10 +0200

    Dodanie kontrolera pacjenta

--- iMedicalAPI/iMedicalAPI/Startup.cs
+++ iMedicalAPI/iMedicalAPI/Startup.cs
@@ -1,7 +1,9 @@
+using iMedicalAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,8 +29,8 @@ namespace iMedicalAPI
         {
 
             services.AddControllers();
+            services.AddDbContext<iMedicalContext>();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
